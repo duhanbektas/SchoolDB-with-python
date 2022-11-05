b603ef41cf34600770ec80799cc65b398b6b9ae0
Duhan <107396918+duhanbektas@users.noreply.github.com>
2022-11-05 16:54:30 +0300

    Update selecting.py

--- selecting.py
+++ selecting.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 def insertProduct(name, price, imageUrl, description):
-    connection = mysql.connector.connect(host="localhost", user = "root", password="mysql1234", database="node_app")
+    connection = mysql.connector.connect(host="localhost", user = "root", password="password", database="node_app")
     cursor = connection.cursor()
 
     sql = "INSERT INTO Products(name,price,imageUrl,description) VALUES (%s,%s,%s,%s)" 
@@ -11,16 +11,16 @@ def insertProduct(name, price, imageUrl, description):
 
     try:
         connection.commit()   
-        print(f'{cursor.rowcount} tane kayıt eklendi')
-        print(f'son eklenen kaydın id: {cursor.lastrowid}')
+        print(f'{cursor.rowcount} record(s) added')
+        print(f'Last record id: {cursor.lastrowid}')
     except mysql.connector.Error as err:
-        print('hata:', err)
+        print('Error:', err)
     finally:
         connection.close()
-        print('database bağlantısı kapandı.')
+        print('DB Connection closed.')
 
 def insertProducts(list):
-    connection = mysql.connector.connect(host="localhost", user = "root", password="mysql1234", database="node_app")
+    connection = mysql.connector.connect(host="localhost", user = "root", password="password", database="node_app")
     cursor = connection.cursor()
 
     sql = "INSERT INTO Products(name,price,imageUrl,description) VALUES (%s,%s,%s,%s)" 
@@ -30,16 +30,16 @@ def insertProducts(list):
 
     try:
         connection.commit()   
-        print(f'{cursor.rowcount} tane kayıt eklendi')
-        print(f'son eklenen kaydın id: {cursor.lastrowid}')
+        print(f'{cursor.rowcount} record(s) added')
+        print(f'Last record id: {cursor.lastrowid}')
     except mysql.connector.Error as err:
-        print('hata:', err)
+        print('Error:', err)
     finally:
         connection.close()
-        print('database bağlantısı kapandı.')
+        print('DB Connection closed.')
 
 def getProducts():
-    connection = mysql.connector.connect(host="localhost", user = "root", password="mysql1234", database="node_app")
+    connection = mysql.connector.connect(host="localhost", user = "root", password="password", database="node_app")
     cursor = connection.cursor()
 
     # cursor.execute('Select * From Products')
