81e182a36df3834710596836f74df67ee29b2f00
Duhan <107396918+duhanbektas@users.noreply.github.com>
2022-11-05 16:51:42 +0300

    Update mysql-connection.py

--- mysql-connection.py
+++ mysql-connection.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host = "localhost", # 192.23.45.56
+    host = "localhost", # 192.23.45.56 -- example // you need to obtain this from a hosting service with money-money-money of course.
     user = "root",
-    password = "mysql1234",
+    password = "password",
     database = "node-app"
 )
 
